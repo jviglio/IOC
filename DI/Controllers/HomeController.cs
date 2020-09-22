c8e063a28b0af482522ac77d845d3a0b26f9f5c3
Juan Viglio <juan.viglio@calibersoftware.com>
2020-09-21 21:00:21 -0300

    Add project files.

---
+++ DI/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DI.Models;
+
+namespace DI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public string Index()
+        {
+            return _employeeRepository.GetEmployee(2).Name;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DI/Models/MockEmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DI.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){Id = 1, Name = "Juan", Department = "d1", Email = "e1"},
+                new Employee(){Id = 2, Name = "Ana", Department = "d2", Email = "e2"},
+                new Employee(){Id = 3, Name = "Ale", Department = "d3", Email = "e3"}
+            };
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.Where(x => x.Id == Id).FirstOrDefault();
+        }
+    }
+}
